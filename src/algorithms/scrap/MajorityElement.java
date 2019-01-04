bb405941a9e46a0bcfa4a1acd3556eb4179fcbbc
郭小川 <gcatml@gmail.com>
2019-01-04 10:32:56 +0800

    day one

---
+++ src/algorithms/scrap/MajorityElement.java
@@ -0,0 +1,13 @@
+package algorithms.scrap;
+
+/**
+ * Created by kadokawa
+ * Date 10:24 4/1/2019
+ */
+public class MajorityElement {
+
+    /*
+        给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+        你可以假设数组是非空的，并且给定的数组总是存在众数。
+     */
+}
