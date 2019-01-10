e4c2993e9be57cb63d473c5829863530636b32c3
郭小川 <gcatml@gmail.com>
2019-01-10 10:30:41 +0800

    no.599

--- src/algorithms/easy/scrap/Minimum_Index_Sum_of_Two_Lists.java
+++ src/algorithms/easy/scrap/Minimum_Index_Sum_of_Two_Lists.java
@@ -1,5 +1,7 @@
 package algorithms.easy.scrap;
 
+import java.util.*;
+
 /**
  * Created by kadokawa
  * Date 16:47 7/1/2019
@@ -30,4 +32,38 @@ public class Minimum_Index_Sum_of_Two_Lists {
         下标从0开始，到列表的长度减1。
         两个列表都没有重复的元素。
      */
+
+    public static void main(String[] args) {
+    //  String[] list1 = new String[]{"k","KFC"};
+    //  String[] list2 = new String[]{"k","KFC"};
+        String[] list1 = new String[]{"Shogun","Tapioca Express","Burger King","KFC"};
+        String[] list2 = new String[]{"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
+
+        System.out.println(Arrays.toString(findRestaurant(list1, list2)));
+    }
+
+    private static String[] findRestaurant(String[] list1, String[] list2) {
+        // 此处使用了 Integer.MAX_VALUE 作为比较的初始值 则只要是第一次进入比较判断 都会进入 赋值cacheCount 以及 设置返回集合的初始内容中
+        // 亦可使用多一个字段去记录 是否更改过比较的初始值。    <初始值直接取0的话，可能出现统计中的最小索引值也为0的情况>
+        int cacheCount = Integer.MAX_VALUE;
+        Vector<String> record = new Vector<>();
+        Map<String,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < list1.length; i++) map.put(list1[i],i);
+
+        for (int i = 0; i < list2.length && i <= cacheCount; i++) {
+            if (map.containsKey(list2[i])) {
+                int count = map.get(list2[i]) + i;
+                if (count < cacheCount) {
+                    record.clear();
+                    record.add(list2[i]);
+                    cacheCount = count;
+                } else if (count == cacheCount) {
+                    record.add(list2[i]);
+                }
+
+            }
+        }
+        return record.toArray(new String[0]);
+    }
 }
